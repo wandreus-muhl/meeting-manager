0628a860eaaf420f4f09dbd3c9af435a1be20c4a
Wandreus <wandreusmuhl70@gmail.com>
2021-12-24 09:12:40 -0400

    Terminando configuração inicial e adicionando .env

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+// Importando controladores
+const meetController = require('./controllers/meetController')
+
+// Rotas
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
+// Importando dependências
+require('dotenv').config
 const express = require('express')
 
+// Configurando app
 const app = express()
 app.use(express.json())
-
 app.use(express.urlencoded({
     extended: true
 }))
+
+// Importando rotas
+// const routes = require('./routes')
+// app.use(routes)
+
+// Iniciando servidor
+app.listen(process.env.API_PORT || 3000, () => {
+    console.log('🚀 - Servidor rodando na porta', process.env.API_PORT || 3000)
+})
+
+// Rota de teste
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Tá online'
+    })
+})
